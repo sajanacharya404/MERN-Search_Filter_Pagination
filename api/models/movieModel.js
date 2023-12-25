8b656f8a8a795e3fe5a377d375036e0a3a85c4d6
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-25 09:54:03 +0545

    Movie model created

---
+++ api/models/movieModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const movieSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  genre: { type: [String], required: true },
+  rating: { type: Number, required: true },
+  img: { type: String, required: true },
+  year: { type: Number, required: true },
+});
+
+const movieModel = mongoose.model("Movie", movieSchema);
+
+export default movieModel;
