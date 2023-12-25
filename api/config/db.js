5af2c7c4390f775c787eceeff3837c1350f26296
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-25 09:47:25 +0545

    Database Connected

---
+++ api/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  mongoose.connect(process.env.MONGO_URI);
+  console.log(`Database connected `);
+};
+export default connectDB;
--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 
 const app = express();
+connectDB();
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
